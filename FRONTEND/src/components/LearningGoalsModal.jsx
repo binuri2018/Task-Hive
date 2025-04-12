73235ee8818c51189328173679983c515687b669
sadulae <sadulaelwitigala@gmail.com>
2025-04-12 20:49:51 +0530

    Added new Icons for the Progress in Learning goals

--- FRONTEND/src/components/LearningGoalsModal.jsx
+++ FRONTEND/src/components/LearningGoalsModal.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { X, Plus, Target, Calendar, Trash, Edit2, CheckSquare, Square, Flag } from "lucide-react";
