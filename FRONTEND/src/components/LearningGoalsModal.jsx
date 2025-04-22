b6d6f365ae2e81792409b4b30455c66b18428764
sadulae <sadulaelwitigala@gmail.com>
2025-04-22 12:07:22 +0530

    Updated Learning Goal component to work with Profile page

--- FRONTEND/src/components/LearningGoalsModal.jsx
+++ FRONTEND/src/components/LearningGoalsModal.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { X, Plus, Target, Calendar, Trash, Edit2, CheckSquare, Square, Flag } from "lucide-react";
